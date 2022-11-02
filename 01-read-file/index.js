2354872b07495ebef10585d5dad32607fb4bf4c7
sanich123 <aovoronin.piano@gmail.com>
2022-11-02 14:19:23 +0300

    feat: work on the first task is done

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const {stdout} = process;
+const path = require('path');
+const fs = require('fs');
+
+fs.readFile(
+    path.join(__dirname, 'text.txt'),
+    'utf-8',
+    (error, data) => {
+        if (error) throw new Error('Что-то пошшо не так');
+        if (data) {
+            stdout.write(data);
+        }
+    }
+)
