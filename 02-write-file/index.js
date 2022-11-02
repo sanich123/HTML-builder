23408b273091805762da91ddcd02a5decfc2b597
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-02 22:45:28 +0300

    feat: work on task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,28 +1,30 @@
 const { stdin, stdout } = process;
 const fs = require("fs");
 const path = require("path");
+const pathName = path.join(__dirname, "text.txt");
 
 const errorHandler = (err) => {
   if (err) {
     throw new Error("Что-то пошло не так");
   }
 };
+const exitMaker = () => {
+  stdout.write("Желаю творческих узбеков и счастья в личной жизни. Пух.");
+  process.exit();
+};
 
-fs.writeFile(path.join(__dirname, "text.txt"), "", errorHandler);
+fs.writeFile(pathName, "", errorHandler);
 
 stdout.write("Введите текст, который нужно сохранить в файл\n");
 
 stdin.on("data", (data) => {
-  fs.appendFile(
-    path.join(__dirname, "text.txt"),
-    data.toString(),
-    errorHandler
-  );
+  if (data.toString().trim() === "exit") {
+    exitMaker();
+  }
+  fs.appendFile(pathName, data.toString(), errorHandler);
   stdout.write("Успешно записалось, бахни еще ченить\n");
-
 });
 
-process.on('SIGINT', () => {
-    stdout.write('Желаю творческих узбеков и счастья в личной жизни. Пух.');
-    process.exit();
+process.on("SIGINT", () => {
+  exitMaker();
 });
