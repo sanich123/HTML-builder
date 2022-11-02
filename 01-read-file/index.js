02de01c49add045b37b2e873e68e0038c48b2d9f
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-02 22:34:16 +0300

    feat: work on readableStream, task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,10 @@
 const { stdout } = process;
 const path = require("path");
 const fs = require("fs");
-
-fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (error, data) => {
-  if (error) {
-    throw new Error("Что-то пошшо не так");
-  }
+const pathName = path.join(__dirname, 'text.txt');
+const stream = new fs.ReadStream(pathName);
+stream.on("readable", () => {
+  const data = stream.read();
   if (data) {
     stdout.write(data);
   }
