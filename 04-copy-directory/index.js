560bd3b1c9961a5a7e6adfbeaa36c91dcbd743a8
sanich123 <aovoronin.piano@gmail.com>
2022-11-03 10:18:29 +0300

    feat: work on task 5, all done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,26 +4,21 @@ const fs = require("fs");
 const copyiedFolder = path.join(__dirname, "files-copy");
 const parentFolder = path.join(__dirname, "files");
 
-fs.mkdir(copyiedFolder, { recursive: true }, (err, data) => {
+const errorHandler = (err) => {
   if (err) {
     throw new Error("Что-то пошло не так");
-  } else {
-    console.log("Папка создана");
   }
-});
+};
 
+fs.mkdir(copyiedFolder, { recursive: true }, errorHandler);
 fs.readdir(parentFolder, (error, data) => {
   if (error) {
     throw new Error("Что-то пошло не так");
   } else {
     data.forEach((file) => {
-        const filePath = path.join(__dirname, 'files', file);
+        const filePath = path.join(parentFolder, file);
         const destinationPath = path.join(copyiedFolder, file);
-      fs.copyFile(filePath, destinationPath, (err) => {
-        if (err) {
-            throw new Error("Что-то пошло не так");
-        }
-      });
+      fs.copyFile(filePath, destinationPath, errorHandler);
     });
   }
 });
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require("fs");
+const errorHandler = (err) => {
+  if (err) {
+    throw new Error("Что-то пошло не так");
+  }
+};
+
+const stylesPath = path.join(__dirname, "styles");
+const bundleFile = path.join(__dirname, "project-dist", "bundle.css");
+fs.writeFile(bundleFile, "", errorHandler);
+
+fs.readdir(stylesPath, { withFileTypes: true }, (err, data) => {
+  if (err) {
+    throw new Error("Что-то пошло не так");
+  } else {
+    data.forEach((file) => {
+      const pathToFile = path.join(stylesPath, file.name);
+      const extName = path.extname(pathToFile);
+
+      if (!file.isDirectory() && extName === ".css") {
+        fs.readFile(pathToFile, "utf-8", (err, data) =>
+          err
+            ? errorHandler(err)
+            : fs.appendFile(bundleFile, data, errorHandler)
+        );
+      }
+    });
+  }
+});
