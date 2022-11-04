f7c0353ffbeadc1708629877d0d919a1fabcef00
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-04 18:05:28 +0300

    refactor: diminish value of functions

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -4,17 +4,16 @@ const templatePath = path.join(__dirname, "template.html");
 const componentsPath = path.join(__dirname, "components");
 const distPath = path.join(__dirname, "project-dist");
 const stylesCss = path.join(distPath, "style.css");
-const stylesPath = path.join(__dirname, 'styles');
-const assetsPath = path.join(__dirname, 'assets');
+const stylesPath = path.join(__dirname, "styles");
+const assetsPath = path.join(__dirname, "assets");
+
 const errorHandler = (err) => {
   if (err) {
     throw err;
   }
 };
 let template = "";
-let articles = "";
-let footer = "";
-let header = "";
+let parts = [];
 
 fs.mkdir(distPath, { recursive: true }, errorHandler);
 fs.writeFile(stylesCss, "", errorHandler);
@@ -35,38 +34,44 @@ fs.readdir(stylesPath, { withFileTypes: true }, (err, data) =>
 );
 const templateStream = fs.createReadStream(templatePath, "utf-8");
 templateStream.on("data", (data) => (template += data));
-templateStream.on("end", readArticles);
+templateStream.on("end", () => {
+  parts = template
+    .slice()
+    .split("\n")
+    .filter((el) => el.includes("{") && el.includes("}"))
+    .map((el) => el.trim().replace(/{|}/gi, ""));
+  template = template.split("\n");
+  readAnything();
+});
 
-function readArticles() {
-  const articlesPath = path.join(componentsPath, "articles.html");
-  const readArticles = fs.createReadStream(articlesPath, "utf-8");
-  readArticles.on("data", (data) => (articles += data));
-  readArticles.on("end", () => {
-    template = template
-      .split("\n")
-      .map((el) => (el.includes("{{articles}}") ? articles : el));
-  });
-  readFooter();
-}
-
-function readFooter() {
-  const footerPath = path.join(componentsPath, "footer.html");
-  const readFooter = fs.createReadStream(footerPath, "utf-8");
-  readFooter.on("data", (data) => (footer += data));
-  readFooter.on("end", () => {
-    template = template.map((el) => (el.includes("{{footer}}") ? footer : el));
-    readHeader();
-  });
-}
-
-function readHeader() {
-  const headerPath = path.join(componentsPath, "header.html");
-  const readHeader = fs.createReadStream(headerPath, "utf-8");
-  readHeader.on("data", (data) => (header += data));
-  readHeader.on("end", () => {
-    template = template
-      .map((el) => (el.includes("{{header}}") ? header : el))
-      .join("");
-    fs.writeFile(path.join(distPath, "index.html"), template, errorHandler);
-  });
+function readAnything() {
+  if (parts.length > 0) {
+    const name = parts.pop();
+    const filePath = path.join(componentsPath, `${name}.html`);
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        readAnything();
+      } else {
+        let insertData = "";
+        const readStream = fs.createReadStream(filePath, "utf-8");
+        readStream.on("data", (data) => (insertData += data));
+        readStream.on("end", () => {
+          template = template.map((el) =>
+            el.includes(`{{${name}}}`) ? insertData : el
+          );
+          if (parts.length > 0) {
+            readAnything();
+          } else {
+            fs.writeFile(
+              path.join(distPath, "index.html"),
+              template.join(""),
+              errorHandler
+            );
+          }
+        });
+      }
+    });
+  } else {
+    return;
+  }
 }
