a4a46a7f08e8b4aa4935565de538c1621918a9ce
sanich123 <aovoronin.piano@gmail.com>
2022-11-05 10:40:24 +0300

    refactor: refactor the code

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+
 const templatePath = path.join(__dirname, "template.html");
 const componentsPath = path.join(__dirname, "components");
 const distPath = path.join(__dirname, "project-dist");
@@ -14,33 +15,23 @@ const assets = path.join(__dirname, "assets");
 const errorHandler = (err) => {
   if (err) {
     console.log(err);
-    throw err;
   }
 };
+
+fs.mkdir(distPath, { recursive: true }, errorHandler);
+
 let template = "";
 let parts = [];
 
-fs.mkdir(distPath, { recursive: true }, errorHandler);
-fs.mkdir(distAssets, { recursive: true }, errorHandler);
-fs.mkdir(distFonts, { recursive: true }, errorHandler);
-fs.mkdir(distImg, { recursive: true }, errorHandler);
-fs.mkdir(distSvg, { recursive: true }, errorHandler);
-copyEverything();
 fs.writeFile(stylesCss, "", errorHandler);
 fs.readdir(stylesPath, { withFileTypes: true }, (err, data) =>
-  err
-    ? errorHandler(err)
-    : data.forEach((file) => {
-        const pathToFile = path.join(stylesPath, file.name);
-        const extName = path.extname(pathToFile);
-        if (!file.isDirectory() && extName === ".css") {
-          fs.readFile(pathToFile, "utf-8", (err, styles) =>
-            err
-              ? errorHandler(err)
-              : fs.appendFile(stylesCss, styles, errorHandler)
-          );
-        }
-      })
+  err ? errorHandler(err) : data.forEach((file) => {
+    const pathToFile = path.join(stylesPath, file.name);
+    const extName = path.extname(pathToFile);
+    if (!file.isDirectory() && extName === ".css") {
+      fs.readFile(pathToFile, "utf-8", (err, styles) => err ? errorHandler(err) : fs.appendFile(stylesCss, styles, errorHandler));
+    }
+  })
 );
 
 const templateStream = fs.createReadStream(templatePath, "utf-8");
@@ -54,6 +45,7 @@ templateStream.on("end", () => {
   template = template.split("\n");
   readAnything();
 });
+copyEverything();
 
 function readAnything() {
   if (parts.length > 0) {
@@ -67,18 +59,8 @@ function readAnything() {
         const readStream = fs.createReadStream(filePath, "utf-8");
         readStream.on("data", (data) => (insertData += data));
         readStream.on("end", () => {
-          template = template.map((el) =>
-            el.includes(`{{${name}}}`) ? insertData : el
-          );
-          if (parts.length > 0) {
-            readAnything();
-          } else {
-            fs.writeFile(
-              path.join(distPath, "index.html"),
-              template.join(""),
-              errorHandler
-            );
-          }
+          template = template.map((el) => el.includes(`{{${name}}}`) ? insertData : el);
+          parts.length > 0 ? readAnything() : fs.writeFile(path.join(distPath, "index.html"), template.join(""), errorHandler);
         });
       }
     });
@@ -88,11 +70,15 @@ function readAnything() {
 }
 
 function copyEverything() {
-  fs.readdir(assets, (err, data) => {
+  fs.mkdir(distAssets, { recursive: true }, errorHandler);
+  fs.mkdir(distFonts, { recursive: true }, errorHandler);
+  fs.mkdir(distImg, { recursive: true }, errorHandler);
+  fs.mkdir(distSvg, { recursive: true }, errorHandler);
+  fs.readdir(assets, (err, folders) => {
     if (err) {
       errorHandler(err);
     } else {
-      data.forEach((folder) => {
+      folders.forEach((folder) => {
         const folderPath = path.join(assets, folder);
         fs.readdir(folderPath, (err, assets) => {
           if (err) {
@@ -101,26 +87,13 @@ function copyEverything() {
             assets.forEach((asset) => {
               const extname = asset.split(".")[1];
               let innerFolder;
-              if (extname === "woff2") {
-                innerFolder = "fonts";
-              }
-              if (extname === "jpg") {
-                innerFolder = "img";
-              }
-              if (extname === "svg") {
-                innerFolder = "svg";
-              }
-              const assetsPath = path.join(
-                __dirname,
-                `assets/${innerFolder}`,
-                asset
-              );
-              const distPath = path.join(
-                __dirname,
-                `project-dist/assets/${innerFolder}`,
-                asset
-              );
-              fs.copyFile(assetsPath, distPath, errorHandler);
+              if (extname === "woff2") innerFolder = "fonts";
+              if (extname === "jpg") innerFolder = "img";
+              if (extname === "svg") innerFolder = "svg";
+              const assetsPath = path.join(__dirname, `assets/${innerFolder}`, asset);
+              const distPath = path.join(__dirname, `project-dist/assets/${innerFolder}`, asset);
+              fs.mkdir(path.join(distAssets, `${innerFolder}`), { recursive: true },
+                (error) => error ? console.log(error) : fs.copyFile(assetsPath, distPath, errorHandler));
             });
           }
         });
