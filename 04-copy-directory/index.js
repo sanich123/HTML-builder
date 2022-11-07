4b940a7779bd391496b3f77fa289c7d4cf33f542
Alexander Voronin <aovoronin.piano@gmail.com>
2022-11-07 22:06:14 +0300

    feat: fix a bug in the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,18 +5,36 @@ const copyiedFolder = path.join(__dirname, "files-copy");
 const parentFolder = path.join(__dirname, "files");
 
 const errorHandler = (err) => {
-  if (err) console.log(err)
+  if (err) console.log(err);
 };
 
 fs.mkdir(copyiedFolder, { recursive: true }, errorHandler);
-fs.readdir(parentFolder, (error, data) => {
-  if (error) {
-    errorHandler(err);
-  } else {
-    data.forEach((file) => {
+unlinkFiles();
+
+function unlinkFiles() {
+  fs.readdir(copyiedFolder, (err, files) => {
+    if (err) errorHandler(err);
+    else {
+      files.forEach((file) => {
+        fs.unlink(path.join(copyiedFolder, file), (err) => {
+          if (err) errorHandler(err);
+        });
+      });
+      copyFiles();
+    }
+  });
+}
+
+function copyFiles() {
+  fs.readdir(parentFolder, (error, data) => {
+    if (error) {
+      errorHandler(err);
+    } else {
+      data.forEach((file) => {
         const filePath = path.join(parentFolder, file);
         const destinationPath = path.join(copyiedFolder, file);
-      fs.copyFile(filePath, destinationPath, errorHandler);
-    });
-  }
-});
+        fs.copyFile(filePath, destinationPath, errorHandler);
+      });
+    }
+  });
+}
